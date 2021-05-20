9f6b5d7b72eda14b23b96f39072e023f603994c1
Danny Rivera <samboard@hotmail.com>
2021-05-20 08:09:20 -0400

    commit tree 05679a59ee423cbe6c234056ae9b97fc4b0c8c4e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(source, callback) {
+    const r = []
+    for (let i = 0; i < source.length; i++) {
+        const theElement = source[i]
+        r.push(callback(theElement))
+    }
+    return r
+}
+
+function reduce(source, callback, startingValue) {
+    let total
+    if (startingValue) {
+        total = startingValue
+        for (let i = 0; i < source.length; i++) {
+            total = callback(source[i], total);
+        }
+        return total
+    } else {
+        total = source [0]
+        for (let i = 1; i < source.length; i++) {
+            total = callback(source[i], total)
+        }
+        return total
+    }
+}
